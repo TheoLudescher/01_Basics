e558d3cebe5d3e953fcefde1d98bf3b455e12b28
TheoLudescher <theo.ludescher@student.htldornbirn.at>
2023-12-14 11:08:58 +0100

    01_Variablen_Operationen_Video_Done

---
+++ JavaScript/01_JS_Variablen_Operationen/script.js
@@ -0,0 +1,20 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A Hat den Wert " + a);
+
+console.log("A Hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+// *c, /, +, -
+
+c = b-a;
+console.log("b - a = " + c);
